56ed94770aa68fe4c9847de4c6a555470b585436
“Valencia <dias.val.jose@gmail.com>
2022-01-16 01:37:30 +0530

    Reverse a string using negative increment operator

--- reverse_string.java
+++ reverse_string.java
@@ -1,7 +1,7 @@
 public class Main
 {
 	public static void main(String[] args) {
-		String string1= "Valencia";
+		String string1="Valencia";
         String reverse_string="";
         char c;
         for( int i=string1.length()-1;i>=0;i--)
