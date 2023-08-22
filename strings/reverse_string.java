7da9da9c8ad341f0c8da84d6a776e7223c88e061
Valencia Dias <dias.val.jose@gmail.com>
2023-08-22 16:10:44 +0530

    Reverse a string using for loop

---
+++ strings/reverse_string.java
@@ -0,0 +1,14 @@
+public class reverse_string
+{
+	public static void main(String[] args) {
+		String string1="Valencia";
+        String reverse_string="";
+        char c;
+        for( int i=0;i<string1.length();i++)
+        {
+            c=string1.charAt(i);
+            reverse_string=c+reverse_string;
+        }
+        System.out.println(reverse_string);
+	    }
+}
