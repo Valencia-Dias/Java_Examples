810082d6c07358271a2b09d822cc05306e7e2056
Valencia Dias <dias.val.jose@gmail.com>
2023-08-23 01:37:59 +0530

    Array codes in java

---
+++ arrays/accessArray.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class accessArray {
+
+	public static void main(String[] args) {
+		String[] s = {"xyz","abc"};
+		for(int i=0;i<s.length;i++) {
+			System.out.println(s[i]);
+			
+		}
+
+	}
+
+}
