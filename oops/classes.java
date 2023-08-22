c398d1412236549b1d2bb4757c72ec67c2709779
Valencia Dias <dias.val.jose@gmail.com>
2023-08-23 01:40:07 +0530

    Object oriented programming codes in java

---
+++ oops/classes.java
@@ -0,0 +1,23 @@
+package oops;
+
+public class classes {
+    int x = 2;
+    int a;
+    int b = 19;
+    String s = "val";
+    Boolean t = true;
+
+
+    public static void main(String[] args) {
+        classes obj = new classes();
+        obj.a = 4;
+        obj.b = 20;
+        System.out.println(obj.x);
+        System.out.println(obj.s);
+        System.out.println(obj.t);
+        System.out.println(obj.a);
+        System.out.println(obj.b);
+
+    }
+    
+}
